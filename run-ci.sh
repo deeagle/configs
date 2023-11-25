ef6411a2a84c00cf48ac287b0ab2616eef941384
Martin deeagle Kock <code@deeagle.de>
2023-11-25 19:04:03 +0100

    ci(dev): add local shellcheck call

--- run-ci.sh
+++ run-ci.sh
@@ -3,6 +3,10 @@
 echo "[INFO] Running CI ..."
 
 echo "[INFO] Linting ..."
+
+echo "[INFO] Lint shell scripts ..."
+find . -type f -name '*.sh' -not -path "./vendor/*" -exec shellcheck {} +
+
 echo "[INFO] Lint markdown files ..."
 docker run --rm -i -v "${PWD}:/workdir" davidanson/markdownlint-cli2 "**/*.md"
 
